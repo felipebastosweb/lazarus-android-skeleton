289223313e223920bd01f57ee29f4dea4e08f6e0
felipe bastos <felipebastosweb@gmail.com>
2022-11-13 08:29:15 -0300

    Add files via upload

---
+++ start_unit.pas
@@ -0,0 +1,30 @@
+{hint: Pascal files location: ...\AppSkeleton\jni }
+unit start_unit;
+
+{$mode delphi}
+
+interface
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes, SysUtils, AndroidWidget;
+  
+type
+  TAndroidModule1 = class(jForm)
+  private
+    {private declarations}
+  public
+    {public declarations}
+  end;
+
+var
+  AndroidModule1: TAndroidModule1;
+
+implementation
+  
+{$R *.lfm}
+  
+
+end.
